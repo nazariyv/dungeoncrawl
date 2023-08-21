eb1369480f65c193bf095c235bba06eb087a9cb3
nazariyv <hi@naz.ooo>
2023-08-21 20:05:00 +0100

    chore: health hud

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,6 +1,7 @@
 mod collisions;
 mod end_turn;
 mod entity_render;
+mod hud;
 mod map_render;
 mod movement;
 mod player_input;
@@ -24,6 +25,7 @@ pub fn build_player_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
@@ -35,6 +37,7 @@ pub fn build_monster_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
