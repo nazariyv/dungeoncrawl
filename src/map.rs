f7de928795a48aab2197d4795ec39caa416d8cc6
naz <hi@naz.ooo>
2023-08-21 14:06:53 +0100

    feat: map builder

--- src/map.rs
+++ src/map.rs
@@ -38,4 +38,22 @@ impl Map {
             }
         }
     }
+
+    pub fn in_bounds(point : Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH
+        && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point : Point) -> bool {
+        Self::in_bounds(point)
+        && self.tiles[map_idx(point.x, point.y)]==TileType::Floor
+    }
+
+    pub fn try_idx(point : Point) -> Option<usize> {
+        if !Self::in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
+        }
+    }
 }
