b4168829772837523a82567fb8ae50cf4dab3445
naz <hi@naz.ooo>
2023-08-21 17:46:22 +0100

    feat: ecs

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
-const NUM_ROOMS: usize = 20;
 
+const NUM_ROOMS: usize = 20;
 pub struct MapBuilder {
     pub map: Map,
     pub rooms: Vec<Rect>,
@@ -22,7 +22,7 @@ impl MapBuilder {
     }
 
     fn fill(&mut self, tile: TileType) {
-        self.map.tiles.iter_mut().for_each(|t| *t=tile);
+        self.map.tiles.iter_mut().for_each(|t| *t = tile);
     }
 
     fn build_random_rooms(&mut self, rng: &mut RandomNumberGenerator) {
@@ -31,7 +31,7 @@ impl MapBuilder {
                 rng.range(1, SCREEN_WIDTH - 10),
                 rng.range(1, SCREEN_HEIGHT - 10),
                 rng.range(2, 10),
-                rng.range(2, 10)
+                rng.range(2, 10),
             );
             let mut overlap = false;
             for r in self.rooms.iter() {
@@ -46,25 +46,26 @@ impl MapBuilder {
                         self.map.tiles[idx] = TileType::Floor;
                     }
                 });
-                self.rooms.push(room);
+
+                self.rooms.push(room)
             }
         }
     }
 
-    fn apply_horizontal_tunnel(&mut self, x1:i32, x2:i32, y:i32) {
-        use std::cmp::{min, max};
-        for x in min(x1, x2) ..= max(x1, x2) {
-            if let Some(idx) = Map::try_idx(Point::new(x, y)) {
-                self.map.tiles[idx] = TileType::Floor;
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+        use std::cmp::{max, min};
+        for x in min(x1, x2)..=max(x1, x2) {
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
+                self.map.tiles[idx as usize] = TileType::Floor;
             }
         }
     }
 
-    fn apply_vertical_tunnel(&mut self, y1:i32, y2:i32, x:i32) {
-        use std::cmp::{min, max};
-        for y in min(y1, y2) ..= max(y1, y2) {
-            if let Some(idx) = Map::try_idx(Point::new(x, y)) {
-                self.map.tiles[idx] = TileType::Floor;
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+        use std::cmp::{max, min};
+        for y in min(y1, y2)..=max(y1, y2) {
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
+                self.map.tiles[idx as usize] = TileType::Floor;
             }
         }
     }
@@ -72,9 +73,11 @@ impl MapBuilder {
     fn build_corridors(&mut self, rng: &mut RandomNumberGenerator) {
         let mut rooms = self.rooms.clone();
         rooms.sort_by(|a, b| a.center().x.cmp(&b.center().x));
+
         for (i, room) in rooms.iter().enumerate().skip(1) {
-            let prev = rooms[i-1].center();
+            let prev = rooms[i - 1].center();
             let new = room.center();
+
             if rng.range(0, 2) == 1 {
                 self.apply_horizontal_tunnel(prev.x, new.x, prev.y);
                 self.apply_vertical_tunnel(prev.y, new.y, new.x);
