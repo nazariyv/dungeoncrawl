fd1210da4397863c738685faa4bf2fb6744a6527
Nazariy Vavryk <hi@naz.ooo>
2023-08-21 21:41:50 +0100

    feat: combat system

--- src/components.rs
+++ src/components.rs
@@ -26,3 +26,12 @@ pub struct Health {
     pub current: i32,
     pub max: i32,
 }
+
+#[derive(Clone, PartialEq)]
+pub struct Name(pub String);
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct WantsToAttack {
+    pub attacker: Entity,
+    pub victim: Entity,
+}
